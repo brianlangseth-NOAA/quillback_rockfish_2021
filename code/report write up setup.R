b7217cc4c36e14b68a7bf9726b442b6f63e3bb9d
brianlangseth-NOAA <brian.langseth@noaa.gov>
2021-01-26 10:32:45 -0800

    Add report writing setup script

---
+++ code/report write up setup.R
@@ -0,0 +1,8 @@
+##
+#Write up document set-up for data moderate quillback report.
+#Obtained from setup pdf located at: 
+#https://github.com/chantelwetzel-noaa/copper_rockfish_2021/tree/master/write_up/sa4ss_guide
+##
+
+remotes::install_github("nwfsc-assess/sa4ss")
+library(sa4ss)
