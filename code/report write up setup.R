9ec11c3a9a414871be699a94af08b3f917a30875
brianlangseth-NOAA <brian.langseth@noaa.gov>
2021-01-29 16:02:03 -0800

    Update setup
    After discussion with chantel

--- code/report write up setup.R
+++ code/report write up setup.R
@@ -6,3 +6,23 @@
 
 remotes::install_github("nwfsc-assess/sa4ss")
 library(sa4ss)
+
+setwd("U:/Stock assessments/quillback_rockfish_2021/write_up/or")
+setwd("C:/Users/Brian.Langseth/Desktop/or/write_up")
+
+#Set up default document sections. May need to delete a few
+sa4ss::draft(authors = c("Brian J. Langseth",
+                         "Chantel R. Wetzel",
+                         "Alison D. Whitman"),
+             species = "Quillback Rockfish",
+             latin = "Sebastes maliger",
+             coast = "Oregon US West",
+             type = c("sa"),
+             create_dir = FALSE,
+             edit = FALSE)
+
+#Read in model, asign whether to creat plots function. Currently also generates tables
+##NOT CURRENTLY WORKING - need to follow up
+sa4ss::read_model(mod_loc = "C:/Users/Brian.Langseth/Desktop/or/3_0_base", create_plots = FALSE)
+
+bookdown::render_book("00a.Rmd", clean = FALSE, output_dir = getwd())
