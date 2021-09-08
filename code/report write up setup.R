57020b1cceec0625fe918638f06ffb434047ba6b
Brian Langseth <brian.langseth@noaa.gov>
2021-09-08 12:03:40 -0700

    Initial files for rebuilding write up

--- code/report write up setup.R
+++ code/report write up setup.R
@@ -143,3 +143,50 @@ if(file.exists("_main.Rmd")){
 }
 bookdown::render_book("00a.Rmd", clean = FALSE, output_dir = getwd())
 
+
+
+
+##--------------------------------CALIFORNIA REBUILDING-----------------------------------##
+
+#detach("package:sa4ss", unload = TRUE)
+#remotes::install_github("nwfsc-assess/sa4ss")
+library(devtools)
+library(sa4ss)
+library(r4ss)
+source_url("https://raw.githubusercontent.com/r4ss/r4ss/main/R/RebuildPlot.R")
+
+# Specify the directory for the document
+setwd("C:/Users/Brian.Langseth/Desktop/ca/rebuilder/write_up")
+
+#Do once: Set up default document sections. May need to delete a few
+sa4ss::draft(authors = c("Brian J. Langseth",
+                         "Chantel R. Wetzel"),
+             species = "Quillback Rockfish",
+             latin = "Sebastes maliger",
+             coast = "California US West",
+             type = c("sa"),
+             create_dir = FALSE,
+             edit = FALSE)
+
+source_url("https://raw.githubusercontent.com/chantelwetzel-noaa/copper_rockfish_2021/master/code/get_values_rebuilder.R")
+
+# Specify the directory where the models are at
+rebuild_dir = "C:/Users/Brian.Langseth/Desktop/ca/rebuilder"
+run <- c(
+  "910_F2020")#,
+  #"920_F2017_2019",
+  #"921_ydecl2021")
+reb <- list()
+for (a in 1:length(run)){
+  reb[[a]]  <- get_values(rebuild_dir = file.path(rebuild_dir, run[a]))
+}
+save(reb, file = file.path(getwd(), "00mod.Rdata"))
+
+
+if(file.exists("_main.Rmd")){
+  file.remove("_main.Rmd")
+}
+# Render the pdf
+bookdown::render_book("00a.Rmd", clean=FALSE, output_dir = getwd())
+
+
